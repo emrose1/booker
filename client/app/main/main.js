a940c1f1d56d912414cc2c4866ef9040a20c65a9
emrose1 <emjrose@gmail.com>
2015-02-08 17:15:31 +0000

    angular-fullstack skeleton

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('fitbookerApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
